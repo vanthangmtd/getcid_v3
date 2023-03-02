6945a0bfb3bf1691a8d33c9bbdfed2fe8a2dbd2a
vanthangmtd <vanthangdhqn@gmail.com>
2023-03-02 16:35:59 +0700

    update

--- assets/js/ckeditor/config.js
+++ assets/js/ckeditor/config.js
@@ -5,15 +5,15 @@
 
 CKEDITOR.editorConfig = function (config) {
     config.toolbar = [
-        { name: 'document', items: ['Save', 'NewPage', 'ExportPdf', 'Preview', 'Print', '-', 'Templates'] },
-        { name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'] },
+        { name: 'basicstyles', items: ['Undo', 'Redo', '-', 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat'] },
+        { name: 'colors', items: ['TextColor', 'BGColor'] },
+        { name: 'paragraph', items: ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'BidiLtr', 'BidiRtl', 'Language'] },
         { name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'Scayt'] },
-        { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat'] },
-        { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language'] },
         { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
         { name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe'] },
-        { name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize'] },
-        { name: 'colors', items: ['TextColor', 'BGColor'] },
+        { name: 'styles', items: ['Font', 'FontSize', 'Format', 'Styles'] },
+        { name: 'clipboard', items: ['Cut', 'Copy', 'Paste', '-', 'PasteText', 'PasteFromWord',  ] },
+        { name: 'document', items: ['Save', 'NewPage', 'ExportPdf', 'Preview', 'Print', '-', 'Templates'] },
     ];
     config.enterMode = CKEDITOR.ENTER_DIV;
     config.shiftEnterMode = CKEDITOR.ENTER_BR;
